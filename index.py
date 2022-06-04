be2938e5faeda19feb62fd05c6ece0be560d80c0
rishabh-modi2 <88582334+rishabh-modi2@users.noreply.github.com>
2022-06-04 11:20:23 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def hello_world(): 
     return 'Hello World!'
 
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
+
 if __name__ == '__main__':
     app.run()
